c8839f87e60048fc53f49ab8915211fd72b5d45f
Gabriel Neto <gneto2818@gmail.com>
2026-05-01 18:13:01 -0300

    salvando dados da ficha nome, divisao, objetivo .. no banco

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 import mysql.connector
 import cohere
+import json
 
 # Carrega as variáveis de ambiente
 load_dotenv()
@@ -13,7 +14,7 @@ db_pass = os.getenv('DB_PASSKEY')
 # Inicializando o cliente Cohere
 co = cohere.Client(api_key)
 
-def buscar_exercicios_do_banco():
+def buscar_exercicios_do_banco(objetivo_usuario):
     try:
         conexao = mysql.connector.connect(
             host='localhost',
@@ -22,40 +23,87 @@ def buscar_exercicios_do_banco():
             database='personal_ai'
         )
         cursor = conexao.cursor()
-        cursor.execute("SELECT nome FROM exercicios")
+        cursor.execute("SELECT nome FROM exercicios;")
+        exercicios_disponiveis = [row[0] for row in cursor.fetchall()]
+
+
+        cursor.execute("SELECT DISTINCT grupamento_muscular FROM exercicios;")
+        grupos_possiveis = [row[0] for row in cursor.fetchall()]
+        grupos_encontrados =[]
         
-        # Pega a primeira coluna de cada linha (o nome)
-        exercicios = [row[0] for row in cursor.fetchall()]
-        
-        cursor.close()
+
+        for grupo in grupos_possiveis:
+            if grupo.lower() in objetivo_usuario.lower():
+                grupos_encontrados.append(grupo)
+        if not grupos_encontrados:
+            return []        
+        format_strings = ','.join(['%s'] * len(grupos_encontrados))
+        querry = f"SELECT nome, grupamento_muscular FROM exercicios WHERE grupamento_muscular in ({format_strings})"
+        cursor.execute(querry,tuple(grupos_encontrados))
+        exercicios = cursor.fetchall()
         conexao.close()
         return exercicios
+        
     except Exception as e:
         print(f'Erro ao conectar no banco: {e}')
         return []
     
+def salvar_ficha_de_treino_no_banco(objetivo,divisao,treino_texto):
+    try:
+        conexao = mysql.connector.connect(
+            host='localhost',
+            user=db_user,
+            password=db_pass,
+            database='personal_ai'
+        )
+        cursor = conexao.cursor()
+        
+        
+           
+        dados_treino = {
+            "objetivo": objetivo,
+            "divisao": divisao,
+            "treino": treino_texto
+        }
+
+        treino_json = json.dumps(dados_treino)
+
+        sql = "INSERT INTO fichas_treino (nome_usuario, objetivo,divisao,treino_json) VALUES (%s, %s, %s, %s)"
+        user_name = input("Digite seu nome para salvar a ficha de treino:\n")
+        values = (user_name, objetivo, divisao, treino_json)
+        cursor.execute(sql, values)
+        conexao.commit()
+        print("Ficha de treino salva com sucesso!")
+        conexao.close()
+    except Exception as e:
+        print(f'Erro ao salvar ficha de treino: {e}')
+
 def gerar_treino_com_ia():
-    # Pegando dados do MySQL
-    lista_disponivel = buscar_exercicios_do_banco()
     
-    if not lista_disponivel:
-        print("Nenhum exercício encontrado. Verifique seu banco de dados.")
+    objetivo = input("Qual seu foco de treino hoje? (EX: Hipertrofia de Peito): ")
+    divisao = input("Qual a divisão do treino? (EX: A,B,C, Fullbody): ")
+    
+    exercicios_filtrados = buscar_exercicios_do_banco(objetivo)
+    
+    if not exercicios_filtrados:
+        print("Não entendi os grupos musculares ou não temos exercícios para eles.")
         return
 
-    exercicios_str = ", ".join(lista_disponivel)
-
-    # Input do usuário
-    objetivo = input("Qual seu foco de treino hoje? (EX: Hipertrofia de Peito): ")
+    lista_texto = ','.join([f"{ex[0]}({ex[1]})" for ex in exercicios_filtrados])
+  
 
     # Engenharia de prompt
-    prompt_ia = f"""Você é um personal trainer de alto nível.
-    O aluno quer: {objetivo}.
-    Você só pode usar exercícios dessa lista: {exercicios_str}.
-    Monte a ficha com:
-    - Nome do Exercício
-    - Séries e Repetições
-    - Dica rápida de execução"""
+    prompt_ia = f"""Você é um Personal Trainer de alto nível.
+    O aluno quer treinar: {objetivo}.
 
+    REGRAS OBRIGATÓRIAS:
+    1. Use APENAS exercícios desta lista: {lista_texto}
+    2. Não inclua exercícios que o aluno NÃO pediu.
+    3. Se o aluno pediu 'Peito', não sujira 'Pernas'.
+
+    Formate o treino assim:
+    - [Nome do Exercício] | [Séries x Repetições] | [Dica rápida de execução]
+    """
     print("\nGerando treino... aguarde...")
 
     try:
@@ -67,9 +115,15 @@ def gerar_treino_com_ia():
 
         print("\n--- SEU TREINO GERADO POR IA ---")
         print(response.text)
+
+
+        salvar_ficha_de_treino_no_banco(objetivo, divisao, response.text)
     except Exception as e:
         print(f"Erro ao gerar treino: {e}")
 
 # Garante que o script rode apenas se executado diretamente
 if __name__ == "__main__":
     gerar_treino_com_ia()
+
+
+
